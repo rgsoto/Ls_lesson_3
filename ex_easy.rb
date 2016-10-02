05b87ebc87fccb9f1cb9696cffbd1b1e825b74db
Reni <rgabrielsoto@gmail.com>
2016-10-02 13:59:53 -0500

    Lesson 3 easy exercises

---
+++ ex_easy.rb
@@ -0,0 +1,53 @@
+# question 1: Either a randomized array or one number from the array.
+
+=begin question 2:
+1. Not equal or false. Can be used in a loop.
+2. Required?
+3. mutates the caller
+4.
+5. Checks if something is included
+6.
+
+Question 3: Replace the word "important" with "urgent" in this string:
+
+advice = "Few things in life are as important as huse training your pet dinosaur."
+
+advice.sub! 'important', 'urgent'
+
+Question 4:
+delete_at - Delete the numbered index, in this case (1) which is 2 since we start at 0 when counting
+
+delete - deletes the actual numbers (1)
+
+Question 5:
+puts(10..100).cover?(42)
+
+Question 6:
+
+famous_words = "Seven years ago..."
+
+famous_words.insert(0, "Four score and ")
+famous_words.prepend("Four score and ")
+
+Question 8:
+flintstones = ["Fred", "Wilma"]
+flintstones << ["Barney", "Betty"]
+flintstones << ["BamBam", "Pebbles"]
+
+flintstones.flatten
+
+Question 9:
+flinstones = {"Fred" =>0, "Wilma" => 1, "Barney" => 2, "Betty" => 3, "BamBam" =>4, "Pebbles" => 5 }
+
+print flinstones.assoc("Barney")
+
+Question 10:
+flintstones = ["Fred", "Barney", "Wilma", "Betty", "Pebbles", "BamBam"]
+
+flintstones_hash = {}
+flintstones.each_with_index do |name, index|
+  flintstone_hash[name] = index
+  end
+=end
+
+
